9e2a25844edf591f87fe031989090dfd05ad2bcd
unknown <andy.gusev@list.ru>
2016-12-17 13:59:13 +0300

    Thread pool first commit.

---
+++ src/main/java/LightFuture.java
@@ -0,0 +1,9 @@
+import java.util.function.Function;
+
+public interface LightFuture<T> {
+    public boolean isReady();
+
+    public T get() throws LightExecutionException, InterruptedException;
+
+    public <U> LightFuture<U> thenApply(Function<T,U> f);
+}
