642ea97ba0654be5393ebfcdbd5ac1ef5179262e
Andrey <andy.gusev@list.ru>
2017-04-13 01:02:54 +0300

    Tests fix

--- src/test/java/ru/spbau/gusev/vcs/VCSTest.java
+++ src/test/java/ru/spbau/gusev/vcs/VCSTest.java
@@ -1,6 +1,8 @@
 package ru.spbau.gusev.vcs;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,6 +12,32 @@ import java.nio.file.Paths;
 import java.util.Collections;
 
 public class VCSTest {
+    private final Path ignorePath = Paths.get(".ignore");
+    private final String ignoredFiles = ".vcs\n" +
+            ".git\n" +
+            "src\n" +
+            "build\n" +
+            "gradle\n" +
+            ".gradle\n" +
+            ".idea\n" +
+            ".travis.yml\n" +
+            ".gitignore\n" +
+            "build.gradle\n" +
+            "settings.gradle\n" +
+            "gradlew\n" +
+            "gradlew.bat\n" +
+            "readme.md\n";
+
+    @Before
+    public void makeIgnore() throws IOException {
+        Files.write(ignorePath, ignoredFiles.getBytes());
+    }
+
+    @After
+    public void delIgnore() throws IOException {
+        Files.deleteIfExists(ignorePath);
+    }
+
     @Test
     public void getCreatedTest() throws IOException, VCS.RepoAlreadyExistsException,
             VCS.BadRepoException {
@@ -71,6 +99,22 @@ public class VCSTest {
 
             Assert.assertEquals(Collections.singletonList(filePath.toString()),
                     VCS.getRemoved());
+        } finally {
+            HashedDirectory.deleteDir(Repository.REPO_DIR_NAME);
+            Files.deleteIfExists(filePath);
+        }
+    }
+
+    @Test
+    public void cleanTest() throws VCS.RepoAlreadyExistsException, IOException,
+            VCS.BadRepoException {
+        Path filePath = Paths.get("file");
+
+        try {
+            Repository.create("usr");
+            Files.createFile(filePath);
+            VCS.clean();
+            Assert.assertTrue(Files.notExists(filePath));
         } finally {
             HashedDirectory.deleteDir(Repository.REPO_DIR_NAME);
             Files.deleteIfExists(filePath);
